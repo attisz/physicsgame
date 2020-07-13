62729580afecbd82afd5569fa78c96ef63c3184e
ati <attila.toth@gmail.com>
2020-07-13 19:48:58 +0200

    add sprites

--- core/src/com/mygdx/game/PhysicsGame.java
+++ core/src/com/mygdx/game/PhysicsGame.java
@@ -3,34 +3,63 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PhysicsGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	TextureAtlas textureAtlas;
+	OrthographicCamera camera;
+	ExtendViewport viewport;
+	final Map<String, Sprite> sprites = new HashMap<String, Sprite>();
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(800, 600, camera);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		Arrays.stream(new int[0]).forEach(System.out::println);
+		textureAtlas = new TextureAtlas("sprites.txt");
+
+		textureAtlas.getRegions().forEach(region -> {
+			Sprite sprite = textureAtlas.createSprite(region.name);
+			sprites.put(region.name, sprite);
+		});
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.57f, 0.77f, 0.85f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		drawSprite("cherries", 100, 100);
+		drawSprite("banana", 0, 0);
+
 		batch.end();
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+		batch.setProjectionMatrix(camera.combined);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		textureAtlas.dispose();
+		sprites.clear();
+	}
+
+	private void drawSprite(String name, float x, float y) {
+		Sprite sprite = sprites.get(name);
+		sprite.setPosition(x, y);
+		sprite.draw(batch);
 	}
 }
